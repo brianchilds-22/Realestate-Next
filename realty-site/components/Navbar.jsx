489d1241645d1de960807b2e064e28ce54daeaea
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-04-30 12:38:59 -0600

    NextAuth-Working

--- realty-site/components/Navbar.jsx
+++ realty-site/components/Navbar.jsx
@@ -10,6 +10,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const profileImage = session?.user?.image || profileDefault;
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [providers, setProviders] = useState(false);
@@ -163,8 +164,10 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
                       alt=""
+                      width={40}
+                      height={40}
                     />
                   </button>
                 </div>
@@ -185,6 +188,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -198,6 +204,10 @@ const Navbar = () => {
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
--- realty-site/utils/authOptions.js
+++ realty-site/utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   providers: [
@@ -18,15 +20,31 @@ export const authOptions = {
     // Invoked on successful signIn
     async signIn({ profile }) {
       // Connect to database
+      await connectDB();
       // Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // If not, add user to database
+      if (!userExists) {
+        //  truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       //Return true to allow sign in
+      return true;
     },
     // Modifies the session object
     async session({ session }) {
       // Get user from database
+      const user = await User.findOne({ email: session.user.email });
       // Assign user Id to the session
+      session.user.id = user._id.toString();
       //   return to session
+      return session;
     },
   },
 };
