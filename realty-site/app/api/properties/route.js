38205256233a1e56fd61e22cf2967441739dbff0
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-05-01 10:09:46 -0600

    AddForm

--- realty-site/app/api/properties/route.js
+++ realty-site/app/api/properties/route.js
@@ -1,5 +1,8 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@utils/authOptions";
+import { connect } from "http2";
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -12,5 +15,59 @@ export const GET = async (request) => {
   } catch (error) {
     console.log(error);
     return new Response("Error", { status: 500 });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const formData = await request.formData();
+
+    // access all values form images and amenities
+    const amenities = formData.getAll("amenities");
+    const images = formData.getAll("images").filter((image) => image !== "");
+
+    // create propertyData object for database
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      bath: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nighly: formData.get("rates.nighly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      images,
+    };
+
+    console.log(propertyData);
+
+    return new Response(JSON.stringify({ message: "Success" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to add property", { status: 500 });
   }
 };
