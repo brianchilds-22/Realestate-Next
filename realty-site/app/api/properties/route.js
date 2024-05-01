a4fb0dc5d63a473d2c131d19fd6d3a6c64680214
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-05-01 13:54:44 -0600

    cloudinary-setup

--- realty-site/app/api/properties/route.js
+++ realty-site/app/api/properties/route.js
@@ -1,8 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@utils/authOptions";
-import { connect } from "http2";
+import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -22,11 +21,13 @@ export const POST = async (request) => {
   try {
     await connectDB();
 
-    const session = await getServerSession(authOptions);
+    const sessionUser = await getSessionUser();
 
-    if (!session) {
-      return new Response("Unauthorized", { status: 401 });
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User Id required", { status: 401 });
     }
+
+    const { userId } = sessionUser;
 
     const formData = await request.formData();
 
@@ -59,14 +60,45 @@ export const POST = async (request) => {
         email: formData.get("seller_info.email"),
         phone: formData.get("seller_info.phone"),
       },
-      images,
+      owner: userId,
     };
 
-    console.log(propertyData);
+    // upload images to cloudinary
+    const imageUploadPromises = [];
 
-    return new Response(JSON.stringify({ message: "Success" }), {
-      status: 200,
-    });
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      // Convert the image data to base64
+      const imageBase64 = imageData.toString("base64");
+
+      // make request to Upload the image to Cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "Realtorready",
+        }
+      );
+      imageUploadPromises.push(result.secure_url);
+
+      // wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+      // Add uploaded images ot property data object
+      propertyData.images = uploadedImages;
+    }
+
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
+
+    // return new Response(JSON.stringify({ message: "Success" }), {
+    //   status: 200,
+    // });
   } catch (error) {
     return new Response("Failed to add property", { status: 500 });
   }
