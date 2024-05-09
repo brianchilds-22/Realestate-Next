6473c90ea53ed08535d40881682c1cc61aaececb
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-05-09 11:16:28 -0600

    MessageSetup

---
+++ realty-site/models/Message.js
@@ -0,0 +1,50 @@
+import exp from "constants";
+import { read } from "fs";
+import { Schema, model, models } from "mongoose";
+import { WEBPACK_RESOURCE_QUERIES } from "next/dist/lib/constants";
+import { type } from "os";
+
+const MessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    property: {
+      type: Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    phone: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = model.Message || model("Message", MessageSchema);
+
+export default Message;
